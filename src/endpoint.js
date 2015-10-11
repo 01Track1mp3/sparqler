977a399fa0dd6b5fd513cfda9c77132d93a39ca5
Sven Mkw <sven.mkw@gmail.com>
2015-10-11 17:14:20 +0200

    build endpoint + tests and added dummy query

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,6 +1,62 @@
+import _ from 'lodash'
+import Query from './query'
+
 
 export default class Endpoint {
-  constructor({ endpointUrl }) {
-    this.endpointUrl = endpointUrl
+  constructor(configs = {}) {
+    const { url = '', prefixes = [], graph = '' } = configs
+
+    if (!_.isString(url)) {
+      throw new Error('Endpoint requires url to be a string.')
+    }
+
+    if (url.length === 0) {
+      throw new Error('Endpoint requires an url.')
+    }
+
+    if (!_.isString(graph)) {
+      throw new Error('Endpoint requires graph to be a string.')
+    }
+
+    if (!_.isArray(prefixes)) {
+      throw new Error('Endpoint requires prefixes to be an array.')
+    }
+
+    this.configs = { url, prefixes, graph }
+  }
+
+
+  //== MUTATING
+
+  setUrl(url) {
+    return new Endpoint({ ...this.configs, url })
+  }
+
+  setGraph(graph) {
+    return new Endpoint({ ...this.configs, graph })
+  }
+
+  setPrefixes(prefixes) {
+    return new Endpoint({ ...this.configs, prefixes })
+  }
+
+  addPrefixes(prefixes) {
+    return this.setPrefixes(this.configs.prefixes.concat(prefixes))
+  }
+
+  addPrefix(prefix) {
+    return this.addPrefixes([ prefix ])
+  }
+
+
+  //== RUN QUERIES
+
+  execute(query) {
+    if (!query || !_.isFunction(query.execute)) {
+      throw new Error('Endpoint requires a query which supports an execute-method.')
+    }
+
+    const { url, graph, prefixes } = this.configs
+    return query.execute(url, graph, prefixes)
   }
 }
