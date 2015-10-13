e1e7da263995968f2b20c356c00d901c5e7a6033
Sven Mkw <sven.mkw@gmail.com>
2015-10-13 23:50:45 +0200

    test fetch on dbpedia

--- __acceptance_tests__/fetchSparqlResponse.js
+++ __acceptance_tests__/fetchSparqlResponse.js
@@ -1,12 +1,21 @@
-describe('fetch sparql response', () => {
-  pit('should fetch results from DBpedia.org', () => {
-    const queryExecution = new QueryExecution('select ?p where { dbr:Carl_Friedrich_Gauss dbo:birthDate ?p }')
-    const _ = require('lodash')
+const _ = require('lodash')
+const QueryExecution = require('../src/QueryExecution')
 
+
+describe('fetch sparql response', () => {
+  pit('should fetch json results from DBpedia.org', () => {
+    const queryExecution = new QueryExecution('select ?p where { dbr:Carl_Friedrich_Gauss dbo:birthDate ?p }')
     return queryExecution
       .privateExecute('http://dbpedia.org/sparql', { format: 'application/json' })
-      .then((response) => {
-        console.log(response)
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.headers.get('Content-Type')).toBe('application/json')
+        expect('body' in response).toBe(true)
+
+        return response.json()
+      })
+      .then(responseJson => {
+        expect(responseJson.results.bindings[0].p.value).toBe('1777-04-30')
       })
   })
 })
